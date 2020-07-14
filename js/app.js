b0b8769a7c75ed8d157a02038afcc4c7cb4d8fa4
Gamra <ilyassstudies2017@gmail.com>
2020-07-14 09:10:43 +0200

    feat(all): contact page + styling

--- js/app.js
+++ js/app.js
@@ -12,3 +12,85 @@ function openNav() {
     hamburger.classList.remove("hamburger-open");
   }
 }
+
+// gallery
+
+// contact
+
+(function($) {
+  "use strict";
+
+  /*FOCUS*/
+  $(".input2").each(function() {
+    $(this).on("blur", function() {
+      if (
+        $(this)
+          .val()
+          .trim() != ""
+      ) {
+        $(this).addClass("has-val");
+      } else {
+        $(this).removeClass("has-val");
+      }
+    });
+  });
+
+  /*VALIDATION*/
+  let name = $('.validate-input input[name="name"]');
+  let email = $('.validate-input input[name="email"]');
+  let message = $('.validate-input textarea[name="message"]');
+
+  $(".validate-form").on("submit", function() {
+    let check = true;
+
+    if (
+      $(name)
+        .val()
+        .trim() == ""
+    ) {
+      showValidate(name);
+      check = false;
+    }
+
+    if (
+      $(email)
+        .val()
+        .trim()
+        .match(
+          /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+        ) == null
+    ) {
+      showValidate(email);
+      check = false;
+    }
+
+    if (
+      $(message)
+        .val()
+        .trim() == ""
+    ) {
+      showValidate(message);
+      check = false;
+    }
+
+    return check;
+  });
+
+  $(".validate-form .input2").each(function() {
+    $(this).focus(function() {
+      hideValidate(this);
+    });
+  });
+
+  function showValidate(input) {
+    let thisAlert = $(input).parent();
+
+    $(thisAlert).addClass("alert-validate");
+  }
+
+  function hideValidate(input) {
+    let thisAlert = $(input).parent();
+
+    $(thisAlert).removeClass("alert-validate");
+  }
+})(jQuery);
